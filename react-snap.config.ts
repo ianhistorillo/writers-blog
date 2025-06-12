33d61ddda21df8fef0ef6a016019ff4f5998b338
ianhistorillo <ian.historillo@gmail.com>
2025-06-13 00:50:58 +0800

    test fix

--- react-snap.config.ts
+++ react-snap.config.ts
@@ -5,7 +5,6 @@ const routes = JSON.parse(fs.readFileSync('./react-snap-routes.json', 'utf-8'));
 export default {
   sourceDir: 'build',
   include: routes,
-  puppeteerArgs: ['--no-sandbox', '--disable-setuid-sandbox'],
   crawl: false, // only crawl the routes you specify
   snapDirectory: true, // IMPORTANT: create folders with index.html inside
 };
